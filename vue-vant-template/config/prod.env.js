10b66822632f9ba97fe534796194339ecc383639
shihongbooo1 <38776365+shihongbooo1@users.noreply.github.com>
2018-12-03 11:06:27 +0800

    Update prod.env.js

--- vue-vant-template/config/prod.env.js
+++ vue-vant-template/config/prod.env.js
@@ -2,7 +2,10 @@
 
 // 生产环境接口配置
 
+const env = process.env.BUILD_TYPE
+let BASE_API_FULL = env === 'prod' ? '"https://m.cjg.emoney.cn/"' : '"http://shmalldev.cps998.com/"'
+
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API_FULL: '"https://m.cjg.emoney.cn//"' // API接口路径
+  BASE_API_FULL: BASE_API_FULL // API接口路径
 }
