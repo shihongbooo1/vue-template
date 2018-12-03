7a7e313b139121b426ce8bb76a1fd093f5a5afaa
shihongbooo1 <38776365+shihongbooo1@users.noreply.github.com>
2018-12-03 13:12:03 +0800

    Update index.js

--- vue-vant-template/src/api/index.js
+++ vue-vant-template/src/api/index.js
@@ -2,6 +2,15 @@ import axios from 'axios'
 import router from '../router'
 import Cookies from 'js-cookie'
 
+let pending = [] // 声明一个数组用于存储每个ajax请求的取消函数和ajax标识
+let removePending = (config) => {
+  for (let p in pending) {
+    if (pending[p].u === config.url + '&' + config.method) { // 当当前请求在数组中存在时执行函数体
+      pending[p].f() // 执行取消操作
+      pending.splice(p, 1) // 把这条记录从数组中移除
+    }
+  }
+}
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.BASE_API_FULL, // 来自config下面配置的api地址
@@ -10,6 +19,7 @@ const service = axios.create({
 
 // request请求拦截器
 service.interceptors.request.use(config => {
+  removePending(config)
   if (config.permission === true) { // config.permission = true 则携带token 等于false 则不携带token
     config.headers['Authorization'] = 'Bearer ' + Cookies.get('token') // 让每个请求携带自定义token 根据实际情况自行修改
   }
@@ -22,6 +32,7 @@ service.interceptors.request.use(config => {
 // respone 请求响应拦截器
 service.interceptors.response.use(config => {
   const res = config.data
+  removePending(config)
   if (res.code === '401') {
     router.push({ path: '/' })
   }
@@ -31,3 +42,4 @@ service.interceptors.response.use(config => {
 })
 
 export default service
+
